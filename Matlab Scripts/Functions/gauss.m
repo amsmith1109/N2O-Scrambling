ed7e4b9bc5b860e2d37714203d31dd5a7e2d0ab7
Alex Smith <amsmith1109@gmail.com>
2022-07-05 14:28:48 -0700

    Update gauss.m

--- Matlab Scripts/Functions/gauss.m
+++ Matlab Scripts/Functions/gauss.m
@@ -1,6 +1,12 @@
 % simple function for getting random numbers that follow a gaussian
 % distribution.
 function out = gauss(size, mu, sigma)
+    if ~exist('mu')
+        mu = 0;
+    end
+    if ~exist('sigma')
+        sigma = 1;
+    end
     X = rand(size);
     out = erfinv(1-2*X)*sigma/2 + mu;
 end
