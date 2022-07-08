49488f694b3c813958a668ccd8ea571a48e1c4ba
Alex Smith <amsmith1109@gmail.com>
2022-07-07 20:26:21 -0700

    Analyze d31 vs d45
    Improved analysis demonstrating the shortcomings of d31 vs d45 slope analysis.

---
+++ Matlab Scripts/One Time Scripts/r31_vs_r45.m
@@ -0,0 +1,86 @@
+%% Set up workspace
+clear all; clc; close all;
+load N2O
+load NO
+load praxair
+
+
+%% N2O Data reduction
+% results are a table with the following:
+% Sample name, R15a, R15b, R17, R15b_doubles
+names = fields(N2O);
+n = numel(names);
+for i = 1:n
+    R = [];
+    I = [];
+    set = N2O.(names{i});
+    for j = 1:numel(set)
+        R(j) = set{j}.R(1);
+        I(j) = mean(set{j}.sample(:,1));
+    end
+    [I, idx] = sort(I);
+    R = R(idx);
+    data.min(i,1) = mean(R(1:2));
+    data.max(i,1) = mean(R(end-3:end-2));
+end
+
+%save('C:\Users\Alex\Documents\GitHub\N2O-Scrambling\Matlab Scripts\Data\Reduced Data\N2O_data_table.mat','N2O_data_table')
+%save('C:\Users\Alex\Documents\GitHub\N2O-Scrambling\Matlab Scripts\Data\Reduced Data\N2O_data.mat','N2O_data')
+
+%% NO Data Reduction
+for i = 1:n
+    R = [];
+    I = [];
+    set = NO.(names{i});
+    for j = 1:numel(set)
+        R(j) = set{j}.R(1);
+        I(j) = mean(set{j}.sample(:,1));
+    end
+    [I, idx] = sort(I);
+    R = R(idx);
+    data.min(i,2) = mean(R(1:2));
+    data.max(i,2) = mean(R(end-3:end-2));
+end
+data.min = sort(data.min);
+data.max = sort(data.max);
+data.min_delta(:,1) = (data.min(:,1)./praxair.rref(2)-1)*1000;
+data.min_delta(:,2) = (data.min(:,2)./praxair.rref(1)-1)*1000;
+data.max_delta(:,1) = (data.max(:,1)./praxair.rref(2)-1)*1000;
+data.max_delta(:,2) = (data.max(:,2)./praxair.rref(1)-1)*1000;
+plot(data.max_delta(:,1), data.max_delta(:,2), 'ro')
+hold on
+plot(data.min_delta(:,1), data.min_delta(:,2), 'b^')
+
+minimum = [data.min_delta(1,1), data.min_delta(1,2)];
+plot(minimum(1), minimum(2),'ko')
+
+y(1) = data.max_delta(end,2);
+y(2) = data.min_delta(end,2);
+x = data.max_delta(end,1);
+
+xlim([0, 1100])
+% text(x,mean(y),'\fontsize{10}\}')
+text(x + 10, mean(y) + 1, '\} 8‰');
+text(minimum(1) + 30 , minimum(2) - 5, 'No enrichment');
+xlabel('\delta^{45} (‰)');
+ax = gca;
+ylabel('\delta^{31} (‰)');
+
+ft = polyfit(data.max_delta(:,1), data.max_delta(:,2), 1);
+ft2 = polyfit(data.min_delta(:,1), data.min_delta(:,2), 1);
+
+xx = [0,1e3];
+yy = polyval(ft, xx);
+yy2 = polyval(ft2, xx);
+plot(xx, yy, 'r--')
+plot(xx, yy2, 'b-.')
+legend([ax.Children(1:2)],...
+    '(\Delta) 250 mV' ,...
+    '(o) 4,000 mV' ,...
+    'Location', 'SouthEast');
+print_settings;
+
+s = @(ref, slope) (slope * ref.rref(1) * (ref.R15b + 1)) / ...
+    (ref.rref(2) + slope * ref.rref(1) * (ref.R15b - ref.R15a));
+
+range = [s(praxair, ft2(1)), s(praxair, ft(1))]
---
+++ Matlab Scripts/One Time Scripts/s_vs_I_residuals.m
@@ -0,0 +1,28 @@
+close all; clear all; clc;
+load Intensity_Residuals
+
+bins = residual.hist(:,1);
+counts = residual.hist(:,2);
+[xData, yData] = prepareCurveData( bins, counts );
+
+% Set up fittype and options.
+ft = fittype( 'gauss1' );
+opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+opts.Display = 'Off';
+opts.Lower = [-Inf -Inf 0];
+opts.StartPoint = [2719 0.00015 0.00102864220494136];
+
+% Fit model to data.
+[fitresult, gof] = fit( xData, yData, ft, opts );
+
+% Plot fit with data.
+histogram(residual.data)
+hold on
+xx = linspace(min(xData),max(xData));
+yy = feval(fitresult, xx);
+h = plot( xx, yy ,'LineWidth',3);
+% Label axes
+xlabel residuals
+
+
+
