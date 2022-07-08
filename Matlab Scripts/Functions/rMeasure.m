3b0361a529b294dc8b6e719237b2033012015d20
Alex Smith <amsmith1109@gmail.com>
2022-07-08 11:04:31 -0700

    Cleanup rMeasure.m

--- Matlab Scripts/Functions/rMeasure.m
+++ Matlab Scripts/Functions/rMeasure.m
@@ -12,12 +12,13 @@ function out = rMeasure(R31, R45, R46, s)
         ((1-s)*R45 - R31 + s*R17);
     
     err_function = @(R17) R46 -(... %R46 - all below
-        R15a(R17)*R15b(R17)...      %15N15N16O
-        + R15a(R17)*R17...          %14N15N17O
-        + R15b(R17)*R17 + ...       %15N14N17O
-        ((1/a)*R17)^(1/b));         %14N14N18O
+        R15a(R17) * R15b(R17)...    %15N15N16O
+        + R15a(R17) * R17...        %14N15N17O
+        + R15b(R17) * R17...        %15N14N17O
+        + ((1/a)*R17)^(1/b));       %14N14N18O
     
     R17 = fzero(err_function, [0,1]);
+    
     out = [R15a(R17),...
         R15b(R17),...
         R17,...
